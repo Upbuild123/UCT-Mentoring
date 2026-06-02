b5cd975257cbb8f23702e0a4d911047564fd745e
Michael Sloyer <michaelsloyer@MacBook-Pro-8.local>
2026-06-02 12:57:15 +0900

    chore: add competency and reflection constants

---
+++ config.py
@@ -0,0 +1,14 @@
+COMPETENCIES = [
+    "Communication",
+    "Problem Solving",
+    "Technical Skills",
+    "Collaboration",
+    "Initiative",
+    "Adaptability",
+]
+
+REFLECTION_QUESTIONS = [
+    "What went well this round?",
+    "What was most challenging?",
+    "What are your goals for next round?",
+]
